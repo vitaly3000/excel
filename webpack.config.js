aa5db12f6506d7ec690c4cce217e370c17570ab2
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-05-19 15:16:26 +0300

    14

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
  context: path.resolve(__dirname, 'src'),
@@ -6,6 +10,30 @@ module.exports = {
  entry: './index.js',
  output: {
   path: path.resolve(__dirname, 'dist'),
-  filename: 'bundle.js'
- }
+  filename: 'bundle.[hash].js'
+ },
+ resolve: {
+  extensions: ['.js'],
+  alias: {
+   '@': path.resolve(__dirname, 'src'),
+   '@core': path.resolve(__dirname, 'src/core'),
+  }
+ },
+ plugins: [
+  new CleanWebpackPlugin(),
+  new HtmlWebpackPlugin({
+   template: 'index.html'
+  }),
+  new CopyPlugin({
+   patterns: [
+    {
+     from: path.resolve(__dirname, 'src/favicon.ico'),
+     to: path.resolve(__dirname, 'dist')
+    }
+   ]
+  }),
+  new MiniCssExtractPlugin({
+   filename: 'bundle.[hash].css'
+  }),
+ ]
 }
