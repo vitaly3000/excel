8b89072b11f5ec0b6d0216507129a00a2bdfeb6f
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-05-22 20:30:22 +0300

    base structure

---
+++ src/components/header/Header.js
@@ -0,0 +1,2 @@
+import { ExcelComponent } from '@core/ExcelComponent';
+export class Header extends ExcelComponent {}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import './scss/index.scss'
+import './scss/index.scss';
+import { Excel } from './components/excel/Excel';
 
-
-console.log('Working!')
+const excel = new Excel('#app', {
+  component: [],
+});
+console.log('Excel', excel);
