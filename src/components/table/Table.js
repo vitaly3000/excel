fe6166a4e710102d243fa02a6937d620cfb5d666
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-05-24 00:44:53 +0300

    add titles in table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,39 +1,9 @@
 import { ExcelComponent } from '@core/ExcelComponent';
+import { createTable } from './table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
   toHTML() {
-    return `
-    <div class="row">
-            <div class="row-info"></div>
-            <div class="row-data">
-              <div class="column">
-                A
-              </div>
-              <div class="column">
-                B
-              </div>
-              <div class="column">
-                C
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="row-info">1</div>
-            <div class="row-data">
-              <div contenteditable="true" class="cell selected">1</div>
-              <div contenteditable="true" class="cell">2</div>
-              <div contenteditable="true" class="cell">3</div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="row-info">2</div>
-            <div class="row-data">
-              <div contenteditable="true" class="cell">1</div>
-              <div contenteditable="true" class="cell">2</div>
-              <div contenteditable="true" class="cell">3</div>
-            </div>
-          </div>
-    `;
+    return createTable();
   }
 }
