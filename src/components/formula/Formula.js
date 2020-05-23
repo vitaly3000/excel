df9f95f5ef63465e5fde940fa442d85299728c9f
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-05-23 15:29:36 +0300

    34

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -2,11 +2,20 @@ import { ExcelComponent } from '@core/ExcelComponent';
 
 export class Formula extends ExcelComponent {
   static className = 'excel__formula';
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input'],
+    });
+  }
   toHTML() {
     return `
     <div class="info">fx</div>
           <div class="input" spellcheck="false" contenteditable></div>
         
     `;
+  }
+  onInput(event) {
+    console.log('Formula: onInput', event);
   }
 }
