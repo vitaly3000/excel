7923aa7caee1c99d2ee5a2ae13beed68b45a70f1
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-05-24 13:26:31 +0300

    finish base framework functional

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -2,19 +2,19 @@ const CODES = {
   A: 65,
   Z: 90,
 };
-// function toCell() {
-//   return `
-//  <div class='cell' contentditable></div>
-//  `;
-// }
+function toCell() {
+  return `
+ <div class='cell' contenteditable="true"></div>
+ `;
+}
 function toColumn(col) {
   return `
  <div class='column'>${col}</div>`;
 }
-function createRow(content) {
+function createRow(index, content) {
   return `
  <div class="row">
- <div class='row-info'></div>
+ <div class='row-info'>${index ? index : ''}</div>
  <div class='row-data'>${content}</div>
  </div>`;
 }
@@ -27,9 +27,10 @@ export function createTable(rowsCount = 15) {
 
   const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
 
-  rows.push(createRow(cols));
+  rows.push(createRow(null, cols));
   for (let i = 0; i < rowsCount; i++) {
-    rows.push(createRow());
+    const cells = new Array(colsCount).fill('').map(toCell).join('');
+    rows.push(createRow(i + 1, cells));
   }
   return rows.join('');
 }
