0ecb1e7270d8c35149e44c0f17d0a36ecfccf5fd
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-05-24 13:43:54 +0300

    Framework start (#3)
    * base structure
    
    * created a template
    
    * updated template
    
    * 33
    
    * 34
    
    * add listener onInput in Formula Component
    
    * add removeAddListeners
    
    * add titles in table
    
    * finish base framework functional

---
+++ src/components/formula/Formula.js
@@ -0,0 +1,23 @@
+import { ExcelComponent } from '@core/ExcelComponent';
+
+export class Formula extends ExcelComponent {
+  static className = 'excel__formula';
+  constructor($root) {
+    super($root, {
+      name: 'Formula',
+      listeners: ['input', 'click'],
+    });
+  }
+  toHTML() {
+    return `
+    <div class="info">fx</div>
+          <div class="input" spellcheck="false" contenteditable></div>
+        
+    `;
+  }
+  onInput(event) {
+    console.log(this.$root);
+    console.log('Formula: onInput', event.target.textContent.trim());
+  }
+  onClick() {}
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import './scss/index.scss'
+import './scss/index.scss';
+import { Header } from '@/components/header/Header';
+import { Toolbar } from '@/components/toolbar/Toolbar';
+import { Formula } from '@/components/formula/Formula';
+import { Table } from '@/components/table/Table';
+import { Excel } from '@/components/excel/Excel';
 
-
-console.log('Working!')
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table],
+});
+excel.render();
