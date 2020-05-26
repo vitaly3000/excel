942f2ed6f526c7cd9ac626c2e818fbd3330f5dc1
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-05-27 01:00:33 +0300

    add navigation

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -2,10 +2,19 @@ const CODES = {
   A: 65,
   Z: 90,
 };
-function toCell(_, col) {
-  return `
- <div class='cell' contenteditable="true" data-col="${col}"></div>
- `;
+
+function toCell(row) {
+  return function (_, col) {
+    return `
+      <div 
+      class='cell' 
+      contenteditable="true" 
+      data-col="${col}" 
+      data-type="cell"
+      data-id ="${row}:${col}"
+      ></div>
+     `;
+  };
 }
 function toColumn(col, index) {
   return `
@@ -37,9 +46,9 @@ export function createTable(rowsCount = 15) {
   const cols = new Array(colsCount).fill('').map(toChar).map(toColumn).join('');
 
   rows.push(createRow(null, cols));
-  for (let i = 0; i < rowsCount; i++) {
-    const cells = new Array(colsCount).fill('').map(toCell).join('');
-    rows.push(createRow(i + 1, cells));
+  for (let row = 0; row < rowsCount; row++) {
+    const cells = new Array(colsCount).fill('').map(toCell(row)).join('');
+    rows.push(createRow(row + 1, cells));
   }
   return rows.join('');
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -22,6 +22,9 @@ class Dom {
   off(eventType, callback) {
     this.$el.removeEventListener(eventType, callback);
   }
+  find(selector) {
+    return $(this.$el.querySelector(selector));
+  }
 
   append(node) {
     if (node instanceof Dom) {
@@ -50,6 +53,26 @@ class Dom {
     Object.keys(styles).forEach((key) => {
       this.$el.style[key] = styles[key];
     });
+  }
+  id(parse) {
+    if (parse) {
+      const parsed = this.id().split(':');
+      return {
+        row: +parsed[0],
+        col: +parsed[1],
+      };
+    }
+    return this.data.id;
+  }
+  focus() {
+    this.$el.focus();
+    return this;
+  }
+  addClass(className) {
+    this.$el.classList.add(className);
+  }
+  removeClass(className) {
+    this.$el.classList.remove(className);
   }
 }
 
