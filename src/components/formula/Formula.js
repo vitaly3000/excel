df8d621a52e583b852f6fd56ee7d726fcfe7609e
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-05-29 13:15:11 +0300

    Table logic (#5)
    * add navigation
    
    * add pattern observer
    
    * 60
    
    * finish base table logic

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,23 +1,39 @@
 import { ExcelComponent } from '@core/ExcelComponent';
+import { $ } from '../../core/dom';
 
 export class Formula extends ExcelComponent {
   static className = 'excel__formula';
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input', 'click'],
+      listeners: ['input', 'keydown'],
+      ...options,
     });
   }
   toHTML() {
     return `
     <div class="info">fx</div>
-          <div class="input" spellcheck="false" contenteditable></div>
-        
+    <div id='formula' class="input" spellcheck="false" contenteditable></div>
     `;
   }
-  onInput(event) {
-    console.log(this.$root);
-    console.log('Formula: onInput', event.target.textContent.trim());
+  init() {
+    super.init();
+    this.$formula = this.$root.find('#formula');
+    this.$on('table:select', ($cell) => {
+      this.$formula.text($cell.text());
+    });
+    this.$on('table:input', ($cell) => {
+      this.$formula.text($cell.text());
+    });
   }
-  onClick() {}
+  onInput(event) {
+    this.$emit('formula:input', $(event.target).text());
+  }
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab'];
+    if (keys.includes(event.key)) {
+      event.preventDefault();
+      this.$emit('formula:done');
+    }
+  }
 }
