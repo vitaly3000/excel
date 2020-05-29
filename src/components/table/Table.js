7f4b0c50be7cff1cb62ace9627a745f8f507e9f2
Vitaly <65219683+Vitaly3000@users.noreply.github.com>
2020-05-29 13:14:21 +0300

    finish base table logic

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -10,7 +10,7 @@ export class Table extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Table',
-      listeners: ['mousedown', 'keydown'],
+      listeners: ['mousedown', 'keydown', 'input'],
       ...options,
     });
   }
@@ -22,11 +22,17 @@ export class Table extends ExcelComponent {
   }
   init() {
     super.init();
-    const $cell = this.$root.find('[data-id="0:0"]');
-    this.selection.select($cell);
+    this.selectCell(this.$root.find('[data-id="0:0"]'));
     this.$on('formula:input', (text) => {
       this.selection.current.text(text);
     });
+    this.$on('formula:done', () => {
+      this.selection.current.focus();
+    });
+  }
+  selectCell($cell) {
+    this.selection.select($cell);
+    this.$emit('table:select', $cell);
   }
   onMousedown(event) {
     if (shouldResize(event)) {
@@ -57,7 +63,10 @@ export class Table extends ExcelComponent {
       event.preventDefault();
       const id = this.selection.current.id(true);
       const $next = this.$root.find(nextSelector(key, id));
-      this.selection.select($next);
+      this.selectCell($next);
     }
+  }
+  onInput(event) {
+    this.$emit('table:input', $(event.target));
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -13,7 +13,14 @@ class Dom {
     return this.$el.outerHTML.trim();
   }
   text(text) {
-    this.$el.textContent = text;
+    if (typeof text === 'string') {
+      this.$el.textContent = text;
+      return this;
+    }
+    if (this.$el.tagName.toLowerCase() === 'input') {
+      return this.$el.value.trim();
+    }
+    return this.$el.textContent.trim();
   }
   clear() {
     this.html('');
@@ -73,6 +80,7 @@ class Dom {
   }
   addClass(className) {
     this.$el.classList.add(className);
+    return this;
   }
   removeClass(className) {
     this.$el.classList.remove(className);
